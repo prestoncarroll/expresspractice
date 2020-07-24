a3fbc04fb0ad112a0b446d27f6f6ba9a0b4be0ab
Preston Carroll <plutusinc1@yahoo.com>
2020-07-23 20:07:24 -0500

    test

--- mybackend/App.js
+++ mybackend/App.js
@@ -1,7 +1,23 @@
-const express = require('express')
+const express = require("express");
+var request = require("request");
+
+
+
 const app = express()
 const port = 4000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/", (req, res) => res.send('Hello World!'))
+app.get("/names", (req, res) => res.send('preston julia baby'))
+
+
+app.get("/getweathertoronto"), (req, res) => {
+    request(
+        "http://api.weatherstack.com/current?access_key=f431fbf6e41eef56fab3f9d11033d017&query=Toronto", function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                res.send(body);
+            }
+        }
+    );
+};
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
